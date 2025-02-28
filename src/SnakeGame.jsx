a645e38a2d60ffc6a9c7d553152e7d99e2783296
Theodore Zisimopoulos <57064762+datcord@users.noreply.github.com>
2025-02-28 18:16:53 +0200

    Add snake game visuals and animations with CSS enhancements

--- src/SnakeGame.jsx
+++ src/SnakeGame.jsx
@@ -1,16 +1,39 @@
 import { useState } from "react";
 import React from "react";
 import "./App.css";
+import bodyImg from "./assets/body.png";
+import headImg from "./assets/head.png";
+import foodImg from "./assets/icon-192.png";
 
 function SnakeGame() {
-  const [bodysize, setbodysize] = useState(5);
-  const body = Array(bodysize).fill("C");
-  const bodyParts = body.map((part) => <span>{part}</span>);
-  const snake = () => {};
+  const [bodysize, setbodysize] = useState(3);
+  const body = Array(bodysize).fill(bodyImg);
+  var i = 0;
+  const bodyParts = body.map((part) => (
+    <img className="bodypart" key={i++} src={part} alt="b" />
+  ));
+  const Snake = () => {
+    return (
+      <div className="snake">
+        {bodyParts}
+        <img className="head" src={headImg} alt="h" />
+      </div>
+    );
+  };
+
+  const Food = () => {
+    return <img className="food" src={foodImg} alt="f" />;
+  };
   return (
-    <div className="game">
-      {bodyParts}
-      <span>H</span>
+    <div
+      onAnimationEnd={() => {
+        if (bodysize <= window.innerHeight) {
+          setbodysize(bodysize + 1);
+        }
+      }}
+      className="game"
+    >
+      <Snake />
     </div>
   );
 }
